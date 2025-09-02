8890a25bdc3e621c2f7a4d2727d6f1f495cfaf9a
Andamlaka <andargachweewawey@gmail.com>
2025-09-02 09:28:53 +0300

    set up finished

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="text-2xl font-bold text-green-600">
+      Andargachew Ewawey
+    </div>
+  )
+}
+
+export default App
